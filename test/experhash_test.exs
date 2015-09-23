58edcc4da805a74eef8bc1d61b69417f05187ae0
Ookami Kenrou <ookamikenrou@gmail.com>
2015-09-23 17:40:39 +0200

    Add test for Hamming distance

--- test/experhash_test.exs
+++ test/experhash_test.exs
@@ -38,5 +38,12 @@ defmodule ExPerHashTest do
 
     assert hash == expected
   end
+
+  test "Hamming Distance", %{server: server} do
+    assert ExPerHash.hamming_distance(server, <<0x8cada78f9d8d0e27 :: 64>>, <<0x8cada78f9d8d0e27 :: 64>>) == {:ok, 0}
+    assert ExPerHash.hamming_distance(server, <<0xffffffffffffffff :: 64>>, <<0x0000000000000000 :: 64>>) == {:ok, 64}
+    assert ExPerHash.hamming_distance(server, <<0x9cada78f9d8d0e27 :: 64>>, <<0x8cada78f9d8d0e27 :: 64>>) == {:ok, 1}
+    assert ExPerHash.hamming_distance(server, <<0x8cada7809d8d0e27 :: 64>>, <<0x8cada78f9d8d0e27 :: 64>>) == {:ok, 4}
+  end
 end
 
