746430a972c1ec4c1d74f2386ac22a33125e5e31
hige no ossan <ookamikenrou@gmail.com>
2018-05-30 22:51:20 +0200

    Prepare for variable hash sizes
    Templates, templates everywhere...
    
    Add support for variable hash sizes to experhash_port. Still not exposed
    on the Elixir side, the fixed-size version will be used.

--- cpp/src/experhash_port.cpp
+++ cpp/src/experhash_port.cpp
@@ -12,6 +12,12 @@
 
 void write_error(std::string type, std::string reason);
 
+template<typename uintX_t, int X>
+std::vector<uintX_t> do_hash(const std::string type, const std::string filename);
+
+template<typename uintX_t>
+void send_hash_response(std::vector<uintX_t> hash);
+
 int main(int argc, char *argv[]) {
   Magick::InitializeMagick(*argv);
 
@@ -49,6 +55,33 @@ int main(int argc, char *argv[]) {
           .encode_atom("ok")
           .encode_binary(hash)
           .write();
+      }
+      else if(arity == 4 && command == "hash") {
+        std::string type = decoder.decode_atom();
+        std::string filename = decoder.decode_binary_string();
+        long hash_size = decoder.decode_long();
+
+        switch(hash_size) {
+          case 8:
+            send_hash_response(do_hash<uint8_t, 8>(type, filename));
+            break;
+
+          case 16:
+            send_hash_response(do_hash<uint16_t, 16>(type, filename));
+            break;
+
+          case 32:
+            send_hash_response(do_hash<uint32_t, 32>(type, filename));
+            break;
+
+          case 64:
+            send_hash_response(do_hash<uint64_t, 64>(type, filename));
+            break;
+
+          default:
+            throw ExPerHash::DecodeError("illegal_hash_size");
+            break;
+        }
       }
       else if(arity == 3 && command == "hamming_distance") {
         std::vector<uint8_t> hash1 = decoder.decode_binary();
@@ -89,6 +122,34 @@ int main(int argc, char *argv[]) {
       write_error("encode_error", ex.what());
     }
   }
+}
+
+template<typename uintX_t, int X>
+std::vector<uintX_t> do_hash(const std::string type, const std::string filename) {
+  if(type == "a") {
+    return ExPerHash::a_hash<uintX_t, X>(filename);
+  }
+  else if(type == "d") {
+    return ExPerHash::d_hash<uintX_t, X>(filename);
+  }
+  else if(type == "dd") {
+    return ExPerHash::dd_hash<uintX_t, X>(filename);
+  }
+  else {
+    throw ExPerHash::DecodeError("unknown_hash_type");
+  }
+}
+
+template<typename uintX_t>
+void send_hash_response(std::vector<uintX_t> hash) {
+  ExPerHash::Encoder response;
+
+  response
+    .encode_version()
+    .encode_tuple_header(2)
+    .encode_atom("ok")
+    .encode_binary(hash)
+    .write();
 }
 
 void write_error(std::string type, std::string reason) {
