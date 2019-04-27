2650347b7592f3e25752c5e42895caded6ebb9d5
hige no ossan <ookamikenrou@gmail.com>
2019-04-27 21:49:58 +0200

    Raise version to 0.2.0
    New Hamming distance code is sufficently different to warrant a version
    bump.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExPerHash.Mixfile do
     [
       app: :experhash,
       name: "ExPerHash",
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
